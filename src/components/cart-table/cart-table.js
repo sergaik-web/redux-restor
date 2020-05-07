85041c7017399610d2860f1e30b6f991a17e5ffd
sergaik-web <seroga8863@gmail.com>
2020-05-07 22:52:51 +0300

    Додел ДЗ

--- src/components/cart-table/cart-table.js
+++ src/components/cart-table/cart-table.js
@@ -4,31 +4,31 @@ import { connect } from "react-redux";
 import { deletedItemCart } from "../../actions";
 
 const CartTable = ({ cartItems, deletedItemCart }) => {
-  const items = cartItems.map((item) => {
-    const { title, id, price, url } = item;
-    return (
-      <div key={id} className="cart__item">
-        <img src={url} className="cart__item-img" alt={title} />
-        <div className="cart__item-title">{title}</div>
-        <div className="cart__item-price">{price}$</div>
-        <div onClick={() => deletedItemCart(id)} className="cart__close">
-          &times;
-        </div>
-      </div>
-    );
-  });
-  console.log(items);
-
   return (
     <>
       <div className="cart__title">Ваш заказ:</div>
-      <div className="cart__list">{items}</div>
+      <div className="cart__list">
+        {cartItems.map((item) => {
+          const { title, id, price, url, counter } = item;
+          return (
+            <div key={id} className="cart__item">
+              <img src={url} className="cart__item-img" alt={title} />
+              <div className="cart__item-title">{title}</div>
+              <div className="cart__item-price">
+                {counter} x {price} = {counter * price}$
+              </div>
+              <div onClick={() => deletedItemCart(id)} className="cart__close">
+                &times;
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log(state.cartItems);
   return { cartItems: state.cartItems };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -29,30 +29,82 @@ const reducer = (state = inicialState, action) => {
       };
 
     case "ADD_ITEM_TO_CART":
-      const newCart = [...state.cartItems, action.payload];
-      console.log(newCart);
+      const { title, price, url, category } = action.payload;
+      const indx = state.cartItems.findIndex((item) => item.title === title);
 
-      return {
-        ...state,
-        cartItems: newCart,
-        totalPrice: newCart.reduce((count, item) => count + item.price, 0),
-      };
+      if (indx === -1) {
+        const newCartItem = {
+          title,
+          price,
+          url,
+          category,
+          counter: 1,
+          id: state.cartItems.length + 1,
+        };
+        const cartArrAddItem = [...state.cartItems, newCartItem];
+
+        return {
+          ...state,
+          cartItems: cartArrAddItem,
+          totalPrice: cartArrAddItem.reduce(
+            (count, item) => count + item.price * item.counter,
+            0
+          ),
+        };
+      } else {
+        let updateItem = state.cartItems[indx];
+        updateItem.counter += 1;
+        const cartArrIncItem = [
+          ...state.cartItems.slice(0, indx),
+          updateItem,
+          ...state.cartItems.slice(indx + 1),
+        ];
+
+        return {
+          ...state,
+          cartItems: cartArrIncItem,
+          totalPrice: cartArrIncItem.reduce(
+            (count, item) => count + item.price * item.counter,
+            0
+          ),
+        };
+      }
 
     case "DELETED_ITEM_CART":
-      console.log(action.id);
       const index = state.cartItems.findIndex((item) => item.id === action.id);
-      console.log(index);
-      const deletedItem = [
-        ...state.cartItems.slice(0, index),
-        ...state.cartItems.slice(index + 1),
-      ];
-      console.log(deletedItem);
 
-      return {
-        ...state,
-        cartItems: deletedItem,
-        totalPrice: deletedItem.reduce((count, item) => count + item.price, 0),
-      };
+      if (state.cartItems[index].counter === 1) {
+        const cartArrDeletedItem = [
+          ...state.cartItems.slice(0, index),
+          ...state.cartItems.slice(index + 1),
+        ];
+
+        return {
+          ...state,
+          cartItems: cartArrDeletedItem,
+          totalPrice: cartArrDeletedItem.reduce(
+            (count, item) => count + item.price * item.counter,
+            0
+          ),
+        };
+      } else {
+        const itemDecCounter = state.cartItems[index];
+        itemDecCounter.counter -= 1;
+        const cartArrDecrementItem = [
+          ...state.cartItems.slice(0, index),
+          itemDecCounter,
+          ...state.cartItems.slice(index + 1),
+        ];
+
+        return {
+          ...state,
+          cartItems: cartArrDecrementItem,
+          totalPrice: cartArrDecrementItem.reduce(
+            (count, item) => count + item.price * item.counter,
+            0
+          ),
+        };
+      }
 
     default:
       return state;
